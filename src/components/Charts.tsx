6cc7e48c4a653b3d0a08fdf72997722bf6fe579a
Antonio Galindo <galindosvq@gmail.com>
2024-05-31 13:31:46 +0200

    feat: Add Charts component for displaying data with Chart.js

---
+++ src/components/Charts.tsx
@@ -0,0 +1,49 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+  },
+};
+
+const labels = ["1", "2", "3", "4", "5", "6", "7"];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      borderColor: "rgba(36, 18, 216, 0.62)",
+      backgroundColor: "rgba(36, 18, 216, 0.2)",
+    },
+  ],
+};
+
+export function Charts() {
+  return <Line options={options} data={data} />;
+}
