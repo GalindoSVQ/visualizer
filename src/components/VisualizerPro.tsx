d455640ed6438fc2969612c3739a34aadef84014
Antonio Galindo <galindosvq@gmail.com>
2024-06-04 13:30:31 +0200

    refactor: Update VisualizerPro component to use new grid data files

--- src/components/VisualizerPro.tsx
+++ src/components/VisualizerPro.tsx
@@ -1,21 +1,26 @@
 import SubsurfaceViewer from "@webviz/subsurface-viewer/dist/SubsurfaceViewer";
-import pointsDataJson from "@/vtk-grid/Simgrid_points.json";
-import polysDataJson from "@/vtk-grid/Simgrid_polys.json";
-import propertiesDataJson from "@/vtk-grid/Simgrid_scalar.json";
-// import simgrid8xIJonly_pointsJson from "@/vtk-grid/Simgrid8xIJonly_points.json";
-// import simgrid8xIJonly_polysJson from "@/vtk-grid/Simgrid8xIJonly_polys.json";
-// import simgrid8xIJonly_scalarJson from "@/vtk-grid/Simgrid8xIJonly_scalar.json";
+
+import grid_points from "@/vtk-grid/grid_points.json";
+import grid_polys from "@/vtk-grid/grid_polys.json";
+import grid_scalar from "@/vtk-grid/grid_scalar.json";
+import volveWells from "@/vtk-grid/volve_wells-demo.json";
 
 export function VisualizerPro() {
   return (
     <SubsurfaceViewer
-      bounds={[456150, 5925800, 467400, 5939500]}
+      bounds={[
+        516041.7200975307, 6178162.917538266, 529933.9489226094,
+        6184617.11714477,
+      ]}
       id="grid-3d"
       layers={[
         {
           "@@type": "AxesLayer",
           ZIncreasingDownwards: false,
-          bounds: [453150, 5925800, -2000, 469400, 5939500, 0],
+          bounds: [
+            516041.7200975307, 6178162.917538266, -2447.356262207031,
+            529933.9489226094, 6184617.11714477, -1955.6937561035156,
+          ],
           id: "axes-layer2",
         },
         {
@@ -25,12 +30,22 @@ export function VisualizerPro() {
           gridLines: true,
           id: "Grid3DLayer",
           material: true,
-          pickable: true,
-          pointsData: pointsDataJson,
-          polysData: polysDataJson,
-          propertiesData: propertiesDataJson,
+          // pickable: true,
+          pointsData: grid_points,
+          polysData: grid_polys,
+          // verticalScale: 20,
+          propertiesData: grid_scalar,
+        },
+        {
+          "@@type": "WellsLayer",
+          ZIncreasingDownwards: false,
+          data: "@@#resources.wellsData",
+          id: "volve-wells",
         },
       ]}
+      resources={{
+        wellsData: volveWells,
+      }}
       triggerHome={0}
       views={{
         layout: [1, 1],
