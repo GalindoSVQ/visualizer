cf5da29dd2062ddaa10cdfd6282fc36fa9b886d9
Antonio Galindo <galindosvq@gmail.com>
2024-06-04 17:32:15 +0200

    refactor: Update VisualizerPro component to use new grid data files

--- src/components/VisualizerPro.tsx
+++ src/components/VisualizerPro.tsx
@@ -1,10 +1,51 @@
 import SubsurfaceViewer from "@webviz/subsurface-viewer/dist/SubsurfaceViewer";
-
-// import grid_points from "@/vtk-grid/rubiales/grid_points.json";
-// import grid_polys from "@/vtk-grid/rubiales/grid_polys.json";
-// import grid_scalar from "@/vtk-grid/rubiales/grid_scalar.json";
+import { useEffect, useState } from "react";
 
 export function VisualizerPro() {
+  const [pointsData, setPointsData] = useState([]);
+  const [polysData, setPolysData] = useState([]);
+  const [propertiesData, setPropertiesData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resPointData = await fetch("/data/rubiales/grid_points.json");
+        const pointData = await resPointData.json();
+
+        const polysData = await fetch("/data/rubiales/grid_polys.json");
+        const polys = await polysData.json();
+
+        const resPropertiesData = await fetch(
+          "/data/rubiales/grid_scalar.json"
+        );
+        const propertiesData = await resPropertiesData.json();
+
+        console.log("polys", polys[0]);
+        console.log("pointData", pointData[0]);
+        console.log("propertiesData", propertiesData[0]);
+
+        setPointsData(pointData);
+        setPolysData(polys);
+        setPropertiesData(propertiesData);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
+
   return (
     <SubsurfaceViewer
       bounds={[
@@ -31,9 +72,9 @@ export function VisualizerPro() {
           material: false,
           // pickable: true,
           // verticalScale: 50,
-          pointsData: [],
-          polysData: [],
-          propertiesData: [],
+          pointsData,
+          polysData,
+          propertiesData,
         },
         // {
         //   "@@type": "WellsLayer",
@@ -52,7 +93,7 @@ export function VisualizerPro() {
         viewports: [
           {
             id: "view_1",
-            show3D: true,
+            show3D: false,
           },
         ],
       }}
